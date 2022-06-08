172560a5b5db772721ec24d88a6f88cd1bd0df80
Vertal <rec321@mail.ru>
2022-06-09 00:54:49 +0300

    Добавлени надпись этикетка Label

---
+++ 00 Начало/00_02_widget_label.py
@@ -0,0 +1,13 @@
+"""
+Widget Label
+
+Надпись - Этикетка
+"""
+from tkinter import *
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+lbl = Label(window, text="Привет")
+lbl.grid(column=0, row=0)
+window.mainloop()
+
