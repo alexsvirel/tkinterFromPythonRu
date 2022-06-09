d8d6098bd42920b9cac8a80e1000e963a7ee8887
Vertal <rec321@mail.ru>
2022-06-09 23:08:58 +0300

    Отключить виджет ввода - состояние disabled

---
+++ 00 Начало/00_10_disabled_entry.py
@@ -0,0 +1,23 @@
+"""
+Отключить виджет ввода - состояние disabled
+"""
+from tkinter import *
+
+
+def clicked():
+    res = "Привет, {}".format(txt.get())
+    lbl.configure(text=res)
+
+
+window = Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+window.geometry('400x250')
+lbl = Label(window, text="Привет")
+lbl.grid(column=0, row=0)
+# Добавляем состояние disabled
+txt = Entry(window, width=10, state='disabled')
+txt.grid(column=1, row=0)
+txt.focus()
+btn = Button(window, text="Клик!", command=clicked)
+btn.grid(column=2, row=0)
+window.mainloop()
