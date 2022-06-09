3a7633be93589532d2a4a10f8b00ef3dce78cb67
Alex_Radon <svirel.org@mail.ru>
2022-06-09 18:59:54 +0300

    Добавление виджета Button (кнопки)

--- 00 Начало/00_04_add_button.py
+++ 00 Начало/00_04_add_button.py
@@ -1,5 +1,5 @@
 """
-Заданы размеры окна при открытии приложения
+Добавление виджета Button (кнопки)
 """
 from tkinter import *
 
@@ -8,4 +8,9 @@ window.title("Добро пожаловать в приложение PythonRu")
 window.geometry('400x250')
 lbl = Label(window, text="Привет", font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Не нажимать!")
+btn.grid(column=1, row=0)
+
 window.mainloop()
+
